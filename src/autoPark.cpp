4f2769690b2db7a49def992947a141f1c1bb4817
zenkx039 <zenkx039@umn.edu>
2013-12-02 18:46:37 -0600

    added prototype scan function from sickReader.cpp

--- src/autoPark.cpp
+++ src/autoPark.cpp
@@ -3,86 +3,119 @@
  *  -  The main program to run the parking function.
  */
 #include "Aria.h"
-#define
-      
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+// Global variables for robot and laser
+ArRobot robot;
+ArSick sick;
+
 /*
- * initialize()
+ * initialize
  * - A function to initialize the robot.
  */
 int initialize() {
-  // Manditory init call
-  Aria::init();
+    
+    // Manditory init call
+    Aria::init();
 
-  // Setup connector
-  //ArSimpleConnector connector(&argc, argv);
-  // connector.parseArgs();
+    // Add the laser device
+    robot.addRangeDevice(&sick);
+    
+    // Setup connector
+    ArSimpleConnector connector(&argc, argv);
+    connector.parseArgs();
 
-  // Add the laser device
-  robot.addRangeDevice(&sick);
-
-  // Open the connection using the defaults, if it fails, exit
-  if ((ret = con.open("/dev/ttyUSB0")) != 0)
+    // Try to connect, exit on failure
+	if (!connector.connectRobot(&robot))
+	{
+		printf("Could not connect to robot... exiting\n");
+		Aria::shutdown();
+		return 1;
+	}
+    printf("Robot: Connected\n");
+    
+    // Start the robot running so that if we lose connection the run stops
+	robot.runAsync(true);
+    
+    // Set up the laser
+    sick.configureShort(false,ArSick::BAUD38400,ArSick::DEGREES180,ArSick::INCREMENT_ONE);
+    connector.setupLaser(&sick);
+    sick.runAsync();
+    
+    // Do a blocking connect, if it fails exit
+    if (!sick.blockingConnect())
     {
-      str = con.getOpenMessage(ret);
-      printf("Open failed: %s\n", str.c_str());
-      Aria::shutdown();
-      return 1;
+        printf("Could not connect to SICK laser... exiting\n");
+        Aria::shutdown();
+        return 1;
     }
+    printf("SICK: Connected\n");
   
-  // Set the robot to use the given connection
-  robot.setDeviceConnection(&con);
-
-  // Do a blocking connect, if it fails exit
-  if (!robot.blockingConnect())
-    {
-      printf("Could not connect to robot... exiting\n");
-      Aria::shutdown();
-      return 1;
-    }
-
-  // Connect to the laser
-  sick.setDeviceConnection(&laserCon);
-  if((ret=laserCon.open("/dev/ttyUSB1")) !=0){
-    Aria::shutdown();
-    return 1;
-  }
-  sick.configureShort(false);
-
-  sick.runAsync();
-  if(!sick.blockingConnect()){
-    printf("Could not get sick...exiting\n");
-    Aria::shutdown();
-    return 1;
-  }
-  printf("We are connected to the laser!");
-  
-  // Return 0 for successful initialization
-  return 0;
+    // Return 0 for successful initialization
+    return 0;
 }
 
-int main(){
-  // Variable declarations
-  int ret;
-  ArRobot robot;
-  ArSick sick;
-  ArSerialConnection laserCon;
-  ArSerialConnection con;
-  std::string str;
+/*
+ * scanForSpace
+ * - A function to search for an open space using the SICK laser.
+ */
+void scanForSpace() {
+    while(cnt<10000)
+	{
+		readings=(list<ArSensorReading *,allocator<ArSensorReading *> > *)sick.getRawReadings();//CurrentBuffer..
+		while (readings==NULL) 	readings=(list<ArSensorReading *,allocator<ArSensorReading *> > *)sick.getRawReadings();
+        
+        t=0;
+        for(it=readings->begin();it!=readings->end(); it++)
+        {
+            laser_dist[t]=(*it)->getRange();
+            laser_angle[t]=-90+t;
+            //cout << "laser angle: " << laser_angle[t] << " laser dist.: " << laser_dist[t] <<" "<<"\n";
+            t++;
+        }
+        cnt++;
+        
+	} //end while 1<2
+    for (t=0; t<181; t++){
+        cout << "laser angle: " << laser_angle[t] << " laser dist.: " << laser_dist[t] <<" "<<"\n";
+    }
+}
 
-  // Initialize the Robot
-  initialize();
+/*
+ * main
+ * - Main function for the parking program.
+ */
+int main() {
+    
+    // Initialize the Robot
+    if (initialize() == 0) {
+        printf("Robot: Initialized\n");
+        printf("SICK: Initialized\n");
+    }
+    else {
+        printf("Initialization failed\n");
+    }
   
-  // Scan for parking space
+    // Scan for parking space
+    scanForSpace();
   
     // Log scan data
   
-  // When parking space is found, execute park function
+    // When parking space is found, execute park function
 
     // Log parameters
 
     // Log status
-
-  return 0;
+    
+    // Shutdown the robot
+    robot.waitForRunExit();
+    Aria::shutdown();
+    return 0;
 }
 
 # EOF #
