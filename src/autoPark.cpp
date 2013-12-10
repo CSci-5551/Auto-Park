3abe1d016820c023063efef1d872bee8643e2229
zenkx039 <zenkx039@umn.edu>
2013-12-09 21:46:41 -0600

    Adding first attempt to find corner 1.

--- src/autoPark.cpp
+++ src/autoPark.cpp
@@ -21,11 +21,20 @@ using namespace std;
 #define TRUE 1
 #define FALSE 0
 
+struct reading {
+    double angle;
+    double distance;
+};
+
 // Global variables for robot and laser
 ArRobot robot;
 ArSick sick;
-double laser_dist[500];
-double laser_angle[500];
+reading reading_array[500];
+reading first_corner;
+reading second_corner;
+reading third_corner;
+reading fourth_corner;
+reading fifth_corner;
 FILE *logfp;
 
 /*
@@ -74,9 +83,6 @@ int initialize(int *argc, char **argv) {
     // Setup laser
     connector.setupLaser(&sick);
     
-    // Create logfile2 (Aria logfile)
-    ArSickLogger logger(&robot, &sick, 300, 25, "testlog.txt", false);
-    
     // Set laser to stop the run if connection is broken
     sick.runAsync();
     
@@ -88,23 +94,16 @@ int initialize(int *argc, char **argv) {
         return 1;
     }
     printf("Laser: Connected\n");
-    
-    // TODO: Setup actions
-    // example - ArActionConstantVelocity constantVelocity("Constant Velocity", 400);
-
-    // TODO: Add the actions
-    // example - robot.addAction(&constantVelocity, 20);
-
 
     return 0;
 }
 
 
 /*
- * scanForSpace
+ * takeReadings
  * - A function to search for an open space using the SICK laser.
  */
-void scanForSpace() {
+void takeReadings() {
     int i;
     double dist, angle;
     std::list<ArPoseWithTime *> *readings;
@@ -121,16 +120,17 @@ void scanForSpace() {
     // Current closest reading within a degree range
     dist = sick.currentReadingPolar(-90, 90, &angle);
     if (dist < sick.getMaxRange())
-        printf("Closest reading %.2f mm away at %.2f degrees\n", dist, angle);
+        fprintf(logfp, "Closest reading %.2f mm away at %.2f degrees\n\n", dist, angle);
     else
-        printf("No close reading.\n");
+        fprintf(logfp, "No close reading.\n\n");
     
-    // Take readings and store angle and distance results in respective arrays
+    // Take readings and store angle and distance results in reading array
     readings = sick.getCurrentBuffer();
     for (it = readings->begin(); it != readings->end(); it++) {
-        laser_dist[i] = (*it)->findDistanceTo(ArPose(0, 0));
-        laser_angle[i] = (*it)->findAngleTo(ArPose(0, 0));
-        fprintf(logfp, "Reading %d:\tLaser Dist: %f\tAngle: %f\n", i, laser_dist[i], laser_angle[i]);
+        reading_array[i].dist = (*it)->findDistanceTo(ArPose(0, 0));
+        reading_array[i].angle = (*it)->findAngleTo(ArPose(0, 0));
+        fprintf(logfp, "Reading %d:\tLaser Dist: %f\tAngle: %f\n",
+                i, reading_array[i].dist, reading_array[i].angle);
         i++;
     }
     
@@ -145,21 +145,47 @@ void scanForSpace() {
 
 
 /*
+ * findCorners
+ * - A function to find the corners of a parking space
+ */
+void findCorners() {
+    // Find the first corner
+    int i = 0;
+    double average;
+    reading *current;
+    reading *next;
+    
+    next = reading_array[0];
+    while (next != NULL) {
+        current = reading_array[i];
+        next = reading_array[i+1];
+        if (next.distance < current.distance && first_corner != NULL) {
+            first_corner.distance = current.distance;
+            first_corner.angle = current.angle;
+            fprintf(logfp, "First Corner: Distance: %f\tAngle: %f\n",
+                    first_corner.distance, first_corner.angle);
+        }
+        i++;
+    }
+    // TODO: Find the second corner
+    
+    // TODO: Find the third corner
+    
+    // TODO: Find the fourth courner
+    
+    // TODO: Find the fifth corner
+    
+    return;
+    
+}
+
+
+/*
  * parkRobot
  * - Function to park the robot.
  */
 void parkRobot() {
-    // TODO: Calculate center of circle one
-    
-    // TODO: Calculate triangle angle
-    
-    // TODO: Calculate x distance to move for alignment
-    
-    // TODO: Back up proper distance
-    
-    // TODO: First circle turn
-    
-    // TODO: Second circle turn
+    // TODO: Execute movements using calculated corners.
     
     return;
 }
@@ -198,9 +224,13 @@ int main(int argc, char **argv) {
         fprintf(logfp, "Initialization failed\n\n");
     }
   
-    // Scan for parking space
+    // Take readings
     fprintf(logfp, "## SCAN FOR SPACE ##\n");
-    scanForSpace();
+    takeReadings();
+    
+    // Calcuate corner angles and distances
+    fprintf(logfp, "## CORNERS ##\n");
+    findCorners();
   
     // When parking space is found, execute park function
     parkRobot();
