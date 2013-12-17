74b08d48eefdc5f4fffa41f322aeacd990de1d24
zenkx039 <zenkx039@umn.edu>
2013-12-17 00:29:47 -0600

    Cleaned up code, added move forward if the robot is still far from ahead car in parking space.

--- src/autoPark.cpp
+++ src/autoPark.cpp
@@ -12,13 +12,14 @@
 using namespace std;
 
 // Constants
-#define MAX_MOVES 5        //Maximum times to move MOVE_DISTANCE and check for new spot
-#define MAX_SCANS 3 //Maximum times to scan for corners at each "initial" location
-#define MOVE_DISTANCE 300.0 //Distance to move before attempting to find corners again
+#define MAX_MOVES 5 // Maximum times to move MOVE_DISTANCE and check for new spot
+#define MAX_SCANS 3 // Maximum times to scan for corners at each "initial" location
+#define MOVE_DISTANCE 300.0 // Distance to move before attempting to find corners again
+#define DEPTH_BOUND 100.0 // Adjust depending on expected depth
+#define WIDTH_BOUND 605.0 // Adjust depending on expected width
 #define TURNING_RADIUS 525.0
 #define ROBOT_RADIUS 227.5
 #define ROBOT_BACK 425.0
-#define DEPTH_BOUND 100.0 //Adjust depending on expected depth
 #define WHEEL_BASE 320.0
 #define MAR_ERR 50.0
 #define VMAX 300.0
@@ -28,6 +29,7 @@ using namespace std;
 #define TRUE 1
 #define FALSE 0
 
+// A struct for laser reading data
 struct reading {
     double angle;
     double distance;
@@ -37,11 +39,9 @@ struct reading {
 ArRobot robot;
 ArSick sick;
 reading reading_array[400];
-reading first_corner;
-reading second_corner;
-reading third_corner;
-reading ahead_corner;
+reading first_corner, second_corner, third_corner, ahead_corner;
 double found_depth, found_width;
+int scan_times;
 FILE *logfp;
 
 /*
@@ -55,10 +55,8 @@ int initialize(int *argc, char **argv) {
     ArArgumentParser parser(argc, argv);
     ArSimpleConnector connector(&parser);
     
+    // Set max trans velocity
     robot.setAbsoluteMaxTransVel(VMAX);
-    
-    //initialize distances to 0 to avoid NULL checks
-    
     
     // Manditory init call
     Aria::init();
@@ -97,7 +95,6 @@ int initialize(int *argc, char **argv) {
     // Set laser to stop the run if connection is broken
     sick.runAsync();
     
-    
     // Do a blocking connect, exit on failure
     if (!sick.blockingConnect())
     {
@@ -118,32 +115,34 @@ int initialize(int *argc, char **argv) {
  * - A function to search for an open space using the SICK laser.
  */
 void takeReadings() {
-    int i;
     std::list<ArPoseWithTime *> *readings;
     std::list<ArPoseWithTime *>::iterator it;
-    
-    // Initialize vars
-    i = 0;
-    printf("Scanning...");
-    ArUtil::sleep(500);
-    
-    //Initialize readings to 0
+    double tempAngle;
+    double tempDist;
+    int numReadings;
+    int i;
+    int j;
+
+    // Initialize data
+    numReadings = 0;
     first_corner.distance = 0;
     second_corner.distance = 0;
     third_corner.distance = 0;
     ahead_corner.distance = 0;
-    for(int i = 0; i<400; i++) {
+    for (i = 0; i < 400; i++)
         reading_array[i].distance = 0;
-    }
     
-    // Lock the laser
+    // Print some info
+    printf("Scanning...");
+    fprintf(logfp, "## LASER READINGS %d ##\n", scan_times);
+    
+    ArUtil::sleep(500);
     sick.lockDevice();
     
-    // Take readings from 90-180 degrees and store angle and distance results in reading array
+    // Take readings from 90-180 degrees and store results in reading array
     readings = sick.getCurrentBuffer();
-    int numReadings = 0;
     for (it = readings->begin(); it != readings->end(); it++) {
-        if((*it)->findAngleTo(ArPose(0, 0)) > 89.9) {
+        if ((*it)->findAngleTo(ArPose(0, 0)) > 89.9) {
             reading_array[i].distance = (*it)->findDistanceTo(ArPose(0, 0));
             reading_array[i].angle = (*it)->findAngleTo(ArPose(0, 0));
             numReadings++;
@@ -151,11 +150,11 @@ void takeReadings() {
         i++;
     }
     
-    //reverse array if first value is 180 instead of 90
+    // Reverse array if first value is 180 instead of 90
     if(reading_array[0].angle > 100.0) {
-        for(int j = 0; j <= numReadings; j++) {
-            double tempAngle = reading_array[numReadings-j].angle;
-            double tempDist = reading_array[numReadings-j].distance;
+        for (j = 0; j <= numReadings; j++) {
+            tempAngle = reading_array[numReadings-j].angle;
+            tempDist = reading_array[numReadings-j].distance;
             reading_array[numReadings-j].angle = reading_array[j].angle;
             reading_array[numReadings-j].distance = reading_array[j].distance;
             reading_array[j].angle = tempAngle;
@@ -163,13 +162,11 @@ void takeReadings() {
         }
     }
     
-    
-    //print readings to log file
-    for(int k = 0; k < numReadings; k++) {
+    // Print readings to log file
+    for (int k = 0; k < numReadings; k++) {
         fprintf(logfp, "Reading %d:\tLaser Dist: %f\tAngle: %f\n",
                 k, reading_array[k].distance, reading_array[k].angle);
     }
-    
     
     // Unlock laser and return
     sick.unlockDevice();
@@ -177,6 +174,7 @@ void takeReadings() {
     puts("");
     fprintf(logfp, "\n");
     printf("done\n");
+    scan_times++;
     return;
 }
 
@@ -186,58 +184,47 @@ void takeReadings() {
  * - A function to find the corners of a parking space
  */
 void findCorners() {
-    int i = 0;
+    int i;
     reading current;
     reading next;
     reading nextnext;
-    //bool behind_car = 0; //TODO add logic to find corners assuming starting behind first car
     
+    i = 0;
     nextnext = reading_array[0];
     
+    // Loop through the reading array
     while (nextnext.distance != 0) {
         current = reading_array[i];
         next = reading_array[i+1];
-        nextnext = reading_array[i+2]; //check against 2 readings instead of 1
+        nextnext = reading_array[i+2];
         
-        //This function occasionally fails and doesn't give the corners.
-        //When it fails the data looks fine... so I'm not sure what's going on.
-        
-        //1st corner assuming starting right next to car #1 && we ccan see the botton corner of car2
+        // Find first corner
         if (((current.distance + DEPTH_BOUND) < next.distance) &&
-            ((current.distance + DEPTH_BOUND) < nextnext.distance) && first_corner.distance == 0) {
+            ((current.distance + DEPTH_BOUND) < nextnext.distance) &&
+            first_corner.distance == 0) {
+            
             first_corner.distance = current.distance;
             first_corner.angle = current.angle;
-            fprintf(logfp, "First Corner: Distance: %f\tAngle: %f\n",
-                    first_corner.distance, first_corner.angle);
         }
-        //2nd corner assuming starting right next to car #1
+        
+        // Find second corner
         if (current.distance > next.distance && current.distance > nextnext.distance
             && first_corner.distance != 0 && second_corner.distance == 0) {
             second_corner.distance = current.distance;
             second_corner.angle = current.angle;
-            fprintf(logfp, "Second Corner: Distance: %f\tAngle: %f\n",
-                    second_corner.distance, second_corner.angle);
+
         }
-        //3rd corner assuming starting right next to car #1
+        
+        // Find third corner
         if (current.distance < next.distance && current.distance < nextnext.distance
             && first_corner.distance != 0 && second_corner.distance != 0) {
             third_corner.distance = current.distance;
             third_corner.angle = current.angle;
-            fprintf(logfp, "Third Corner: Distance: %f\tAngle: %f\n\n",
-                    third_corner.distance, third_corner.angle);
             break; //Got all the corners no need to check the other values
         }
         i++;
     }
-    /*
-     Find the first corner if behind first car
-     if (current.distance > next.distance && first_corner.distance == NULL) {
-     first_corner.distance = current.distance;
-     first_corner.angle = current.angle;
-     fprintf(logfp, "First Corner: Distance: %f\tAngle: %f\n",
-     first_corner.distance, first_corner.angle);
-     }
-     */
+
     return;
 }
 
@@ -263,6 +250,7 @@ void findAheadCorner() {
             && ahead_corner.distance == 0) {
             ahead_corner.distance = current.distance;
             ahead_corner.angle = current.angle;
+            fprintf(logfp, "\n## FORWARD MOVE CORRECTION ##\n");
             fprintf(logfp, "Ahead Corner: Distance: %f\tAngle: %f\n",
                     ahead_corner.distance, ahead_corner.angle);
         }
@@ -275,17 +263,16 @@ void findAheadCorner() {
  * - Function to get Depth and Width using Cosines
  */
 void getDimensions() {
-    
     found_depth = sqrt(pow(second_corner.distance,2.0) + pow(third_corner.distance,2.0)
                        - 2.0 * second_corner.distance * third_corner.distance
                        * cos((third_corner.angle - second_corner.angle) * PI / 180));
-    
-    fprintf(logfp, "Depth: %f\n", found_depth);
-    
     found_width = sqrt(pow(first_corner.distance,2.0) + pow(third_corner.distance,2.0)
                        - 2.0 * first_corner.distance * third_corner.distance
                        * cos((third_corner.angle - first_corner.angle) * PI / 180));
-    fprintf(logfp, "Width: %f\n", found_width);
+    
+    // Print results to logfile
+    fprintf(logfp, "Depth: %f\n", found_depth);
+    fprintf(logfp, "Width: %f\n\n", found_width);
     
     return;
 }
@@ -296,68 +283,75 @@ void getDimensions() {
  * - Function to park the robot.
  */
 void parkRobot() {
-    // TODO: Combine variables once we know they are individually correct
+    double first_car_x, ahead_car_x;
+    double circle1_x, circle1_y,
+    double circle2_x, circle2_y;
+    double xtangent, wall_y, wheel_ratio;
+    double right_vel;
+    double turn_angle;
+    double distance_forward;
     
-    double first_car_x = -cos(first_corner.angle * PI /180.0) * first_corner.distance;
+    // Calculate values
+    first_car_x = -cos(first_corner.angle * PI /180.0) * first_corner.distance;
+    wall_y = -sin(second_corner.angle * PI /180.0) * second_corner.distance;
+    circle1_x = first_car_x + ROBOT_BACK;
+    circle1_y = wall_y + ROBOT_RADIUS + TURNING_RADIUS + 30.0;  //50 = wiggle room, mm
+    circle2_y = -TURNING_RADIUS;
+    xtangent = circle1_x+sqrt(pow(TURNING_RADIUS,2.0)-pow(((circle2_y-circle1_y)/2),2.0));
+    circle2_x = (2.0 * xtangent) - circle1_x;
+    wheel_ratio = ((2.0*TURNING_RADIUS/WHEEL_BASE)+1.0)/((2.0*TURNING_RADIUS/WHEEL_BASE)-1.0);
+    right_vel = -(VMAX * 2.0) / (1 + wheel_ratio);
+    turn_angle = atan2(circle1_y - circle2_y, circle2_x - circle1_x);
+    turn_time = 1000 * abs(((PI/2)-A)/((rightVel - wheel_ratio * right_vel)/WHEEL_BASE));
+    
+    // Print calculations to logfile
+    fprintf(logfp, "## CALCULATIONS ##\n");
     fprintf(logfp, "first_car_x: %f\n", first_car_x);
-    
-    double wall_y = -sin(second_corner.angle * PI /180.0) * second_corner.distance;
-    fprintf(logfp, "wall_y %f\n", wall_y);
-    
-    double circle1_x = first_car_x + ROBOT_BACK;
+    fprintf(logfp, "wall_y: %f\n", wall_y);
     fprintf(logfp, "circle1_x: %f\n", circle1_x);
+    fprintf(logfp, "circle1_y: %f\n", circle1_y);
+    fprintf(logfp, "circle2_y: %f\n", circle2_y);
+    fprintf(logfp, "xtangent: %f\n", xtangent);
+    fprintf(logfp, "circle2_x: %f\n", circle2_x);
+    fprintf(logfp, "wheel_ratio: %f\n", wheel_ratio);
+    fprintf(logfp, "right_vel: %f\n", right_vel);
+    fprintf(logfp, "turn_angle: %f\n", turn_angle);
+    fprintf(logfp, "turn_time: %f\n", turn_time);
     
-    double circle1_y = wall_y + ROBOT_RADIUS + TURNING_RADIUS + 30.0;  //50 = wiggle room, mm
-    fprintf(logfp, "circle1_y %f\n", circle1_y);
+    fprintf(logfp, "## MOVE INFO ##\n");
     
-    double circle2_y = -TURNING_RADIUS;
-    fprintf(logfp, "circle2_y %f\n", circle2_y);
-    
-    double xtangent = circle1_x + sqrt(pow(TURNING_RADIUS,2.0) - pow(((circle2_y - circle1_y)/2),2.0));
-    fprintf(logfp, "xtangent %f\n", xtangent);
-    
-    double circle2_x = (2.0 * xtangent) - circle1_x;
-    fprintf(logfp, "circle2_x %f\n", circle2_x);
-    
-    double wheel_ratio = ((2.0 * TURNING_RADIUS/WHEEL_BASE) + 1.0)/((2.0 * TURNING_RADIUS/WHEEL_BASE) - 1.0);
-    fprintf(logfp, "wheel_ratio %f\n", wheel_ratio);
-    
-    double rightVel = -(VMAX * 2.0) / (1 + wheel_ratio);
-    fprintf(logfp, "rightVel %f\n", rightVel);
-    
-    double A = atan2(circle1_y - circle2_y, circle2_x - circle1_x);
-    fprintf(logfp, "turnAngle %f\n", A);
-    
-    double t_turn = 1000 * abs(((PI/2)-A)/((rightVel - wheel_ratio * rightVel)/WHEEL_BASE));
-    fprintf(logfp, "turn_time %f\n", t_turn);
-    
-    //move to starting location for parking
-    cout << "Moving forward " << circle2_x << " mm." << endl;
+    // Move to starting location for parking
+    printf("Moving forward");
+    fprintf(logfp, "Forward move distance: %f mm\n", circle2_x);
     robot.lock();
     robot.move(circle2_x-150);
     robot.unlock();
+    
+    // Wait until move is completed
     ArUtil::sleep(5000);
-    while(robot.isMoveDone() == false) {} //don't do anything until the move is done
+    while(robot.isMoveDone() == false) {}
     
-    //This does the general parking motion, although far from perfect
-    //the ratio seems a bit off because it needs to turn almost 90 degrees to get deep enough into the spot
-    //need to calculate sleep amount or find another way to follow circular path
-    
-    cout << "1st turning velocity: (" << rightVel << "," << (wheel_ratio * rightVel) << ")" << endl;
+    // Make first turn
+    printf("Peforming first turn");
+    fprintf(logfp, "First turn velocity: (%f, %f)\n", right_vel, wheel_ratio * right_vel);
     robot.lock();
-    robot.setVel2((wheel_ratio * rightVel), rightVel);
+    robot.setVel2((wheel_ratio * right_vel), right_vel);
     robot.unlock();
-    ArUtil::sleep(t_turn);
+    ArUtil::sleep(turn_time);
     
-    cout << "2nd turning velocity: (" << (wheel_ratio * rightVel) << "," << rightVel << ")" << endl;
+    // Make second turn
+    printf("Performing second turn");
+    fprintf(logfp, "Second turn velocity: (%f, %f)\n", wheel_ratio * right_vel, right_vel);
     robot.lock();
-    robot.setVel2(rightVel, (wheel_ratio * rightVel));
+    robot.setVel2(right_vel, (wheel_ratio * right_vel));
     robot.unlock();
-    ArUtil::sleep(t_turn);
+    ArUtil::sleep(turn_time);
     
+    // Stop the robot
     robot.lock();
     robot.stop();
     robot.unlock();
+    ArUtil::sleep(2000);
     
     // Take new readings
     takeReadings();
@@ -366,16 +360,19 @@ void parkRobot() {
     findAheadCorner();
     
     // Calculate distance to move forward
-    double ahead_car_x = sin(ahead_corner.angle * PI /180.0) * ahead_corner.distance;
-    fprintf(logfp, "ahead_car_x: %f\n", ahead_car_x);
-    double distance_forward = ahead_car_x - (found_width / 2) + (MAR_ERR / 2) ;
+    ahead_car_x = sin(ahead_corner.angle * PI /180.0) * ahead_corner.distance;
     
-    // Move robot forward
-    robot.lock();
-    robot.move(distance_forward);
-    robot.unlock();
-    ArUtil::sleep(2000);
-    while(robot.isMoveDone() == false) {}
+    // Move forward if still far away from ahead car
+    if (ahead_car_x > (ROBOT_RADIUS + 200)) {
+        printf("Moving forward");
+        fprintf(logfp, "Forward move correction: %f\n", distance_forward);
+        robot.lock();
+        robot.setVel2(100,100);
+        robot.move(ROBOT_RADIUS);
+        robot.unlock();
+        ArUtil::sleep(2000);
+        while(robot.isMoveDone() == false) {}
+    }
     
     return;
 }
@@ -400,6 +397,14 @@ void openLogFile() {
  * - Main function for the parking program.
  */
 int main(int argc, char **argv) {
+    int max_tries;
+    int max_move;
+    bool found_spot;
+    
+    max_tries = MAX_SCANS;
+    max_move = MAX_MOVES;
+    found_spot = false;
+    scan_times = 1;
     
     // Open the logfile
     openLogFile();
@@ -414,19 +419,10 @@ int main(int argc, char **argv) {
         fprintf(logfp, "Initialization failed\n\n");
     }
     
-    // Take readings
-    fprintf(logfp, "## SCAN FOR SPACE ##\n");
-    takeReadings();
-    
-    // Calcuate corner angles and distances
-    fprintf(logfp, "## CORNERS ##\n");
-    findCorners();
-    
-    int max_tries; //Didn't find corners? try a few more times.
-    int max_move = MAX_MOVES;
-    bool found_spot = false;
-    while(!found_spot && max_move > 0) {
-        max_tries = MAX_SCANS;
+    // Find the corners
+    while (!found_spot && max_move > 0) {
+        
+        // Try to find corners from current position
         while((third_corner.distance == 0 || first_corner.angle > 150.0 )&& max_tries > 0) {
             takeReadings();
             findCorners();
@@ -434,31 +430,44 @@ int main(int argc, char **argv) {
                 found_spot = true;
             max_tries--;
         }
+        
+        // Move the robot forward and try again
         robot.lock();
         robot.move(MOVE_DISTANCE);
         robot.unlock();
+        
         ArUtil::sleep(2000);
         while(robot.isMoveDone() == false) {}
+        
+        // Reset position to 0,0
         robot.lock();
-        robot.moveTo(ArPose(0,0,0), true); //resets pose to 0,0 for new position
+        robot.moveTo(ArPose(0,0,0), true);
         robot.unlock();
+        
         ArUtil::sleep(200);
         max_move--;
     }
     
+    // Print corners to logfile
+    fprintf(logfp, "## CORNERS ##\n");
+    fprintf(logfp, "First Corner: Distance: %f\tAngle: %f\n",
+            first_corner.distance, first_corner.angle);
+    fprintf(logfp, "Second Corner: Distance: %f\tAngle: %f\n",
+            second_corner.distance, second_corner.angle);
+    fprintf(logfp, "Third Corner: Distance: %f\tAngle: %f\n\n",
+            third_corner.distance, third_corner.angle);
     
     // Use corners to get dimension of parking spot
+    fprintf(logfp, "## SPACE DIMENSIONS ##\n");
     getDimensions();
     
-    // When parking space is found, execute park function
-    if((found_width > (ROBOT_RADIUS *2 + 150)) && found_spot)
+    // If parking space fits our requirements, execute park function
+    if (found_width > WIDTH_BOUND && found_spot)
         parkRobot();
     else
-        cout << "Adequate spot not found." << endl;
-    cout << found_spot << endl;
+        printf("Adequate spot not found.\n");
     
     // Shutdown the robot
-    //robot.waitForRunExit();
     Aria::shutdown();
     fclose(logfp);
     return 0;
